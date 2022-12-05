3653f8be1b610cdced3b9d487cfd1d154402a682
raghubhogireddy <raghurambhogireddy@gmail.com>
2022-12-05 21:43:31 +0530

    Added more challenges

---
+++ OOPS/src/io/codr/problems/MegaBytesConverter.java
@@ -0,0 +1,23 @@
+package io.codr.problems;
+
+public class MegaBytesConverter {
+    public static void main(String[] args){
+        printMegaBytesAndKiloBytes(2500);
+    }
+
+    private static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+        }else{
+            double megaBytes = convertKiloBytesToMegaBytes(kiloBytes);
+            int remainingKiloBytes = 0;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainingKiloBytes +" KB");
+        }
+
+    }
+
+    private static double convertKiloBytesToMegaBytes(int kiloBytes) {
+        // 1MB = 1024Kb
+        return kiloBytes/1024;
+    }
+}
