33ee64ee5ce7b8a766b9c0fa1c1639a793ebe872
raghubhogireddy <raghurambhogireddy@gmail.com>
2023-02-09 22:34:30 +0530

    Added more code

--- problems/MinutesToYearsDaysCalculator.java
+++ problems/MinutesToYearsDaysCalculator.java
@@ -4,16 +4,20 @@ public class MinutesToYearsDaysCalculator {
     }
 
     public static void printYearsAndDays(long minutes){
-        if (minutes < 0)
+        if (minutes < 0){
             System.out.println("Invalid Value");
-        /*
+        }else {
+            /*
             1 hour = 60 minutes
             1 day = 24 hours = 24 * 60
             1 year = 365 days = 365 * 24 * 60
          */
 
-        long years = minutes / (365 * 24 * 60);
-        long days = minutes % (24 * 60) ;
-        System.out.println(minutes + " min = " + years + " y and " + days +" d");
+            long years = minutes / (365 * 24 * 60);
+            long remainingMinutes = minutes % (365 * 24 * 60);
+            long days = remainingMinutes / (24 * 60);
+            System.out.println(minutes + " min = " + years + " y and " + days +" d");
+        }
+
     }
 }
