667d72f9df658c1057956d31618fe3b38baaa359
raghubhogireddy <raghurambhogireddy@gmail.com>
2023-02-13 22:57:02 +0530

    day10: modified some code

--- problems/PlayingCat.java
+++ problems/PlayingCat.java
@@ -1,6 +1,6 @@
 public class PlayingCat {
     public static void main(String[] args) {
-        System.out.println(isCatPlaying(false, 45));
+        System.out.println( isCatPlaying(false, 45));
     }
 
     public static boolean isCatPlaying(boolean summer, int temperature){
